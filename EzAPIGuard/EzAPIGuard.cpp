37d68d929211a8649f81276f3dbb6baf8e41ebb4
xketx <zpengkee@gmail.com>
2024-02-27 16:49:44 +0800

    init repo

---
+++ EzAPIGuard/EzAPIGuard.cpp
@@ -0,0 +1,66 @@
+﻿// EzAPIGuard.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <Windows.h>
+#include <stdio.h>
+#include <string.h>
+
+BOOL inject(DWORD dwPID, LPCSTR dllPath)
+{
+	HANDLE hProcess = NULL;
+	HANDLE hThread = NULL;
+	HMODULE hMod = NULL;
+	LPVOID pRemoteBuf = NULL;
+	DWORD dwBufSize = (DWORD)strlen(dllPath) * sizeof(char) + 1;
+	LPTHREAD_START_ROUTINE pThreadProc;
+	if (!(hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, dwPID)))
+	{
+		printf("open process %d failed\n", dwPID);
+		return FALSE;
+	}
+	printf("open process success\n");
+	pRemoteBuf = VirtualAllocEx(hProcess, NULL, dwBufSize, MEM_COMMIT, PAGE_READWRITE);
+	if (pRemoteBuf == NULL)
+	{
+		printf("alloc memory failed\n");
+		return FALSE;
+	}
+	printf("memory alloced at 0x%x\n", pRemoteBuf);
+	if (WriteProcessMemory(hProcess, pRemoteBuf, dllPath, dwBufSize, NULL) == FALSE)
+	{
+		printf("write dllPath to process failed\n");
+		return FALSE;
+	}
+	printf("write dllPath success\n");
+	hMod = GetModuleHandleA((LPCSTR)"kernel32.dll");
+	pThreadProc = (LPTHREAD_START_ROUTINE)GetProcAddress(hMod, "LoadLibraryA");
+	if (pThreadProc == NULL)
+	{
+		printf("get LoadLibraryA address failed\n");
+		return FALSE;
+	}
+	printf("LoadLibraA at 0x%x\n", pThreadProc);
+	hThread = CreateRemoteThread(hProcess, NULL, 0, pThreadProc, pRemoteBuf, 0, NULL);
+	if (hThread == INVALID_HANDLE_VALUE)
+	{
+		printf("create remote thread failed\n");
+	}
+	printf("inject success\n");
+	return TRUE;
+}
+
+int main(int argc, const char* argv[])
+{
+	if (argc != 3)
+	{
+		printf("usage: %s process_pid dll_path", argv[0]);
+	}
+	else
+	{
+		DWORD dwPID;
+		LPCSTR dllPath = argv[2];
+		sscanf_s(argv[1], "%d", &dwPID);
+		inject(dwPID, dllPath);
+	}
+	return 0;
+}
---
+++ GuardDll/dllmain.cpp
@@ -0,0 +1,47 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+#include "stdio.h"
+#include "detours.h"
+#include "Windows.h"
+
+#pragma comment(lib, "detours.lib")
+
+static int (WINAPI* OldMessageBoxA)(_In_opt_ HWND hWnd, _In_opt_ LPCSTR lpText, _In_opt_ LPCSTR lpCaption, _In_ UINT uType) = MessageBoxA;
+
+extern "C" __declspec(dllexport) int WINAPI NewMessageBoxA(_In_opt_ HWND hWnd, _In_opt_ LPCSTR lpText, _In_opt_ LPCSTR lpCaption, _In_ UINT uType)
+{
+    printf("\n\n******************************************\n");
+    printf("MessageBoxA Hooked!\n");
+    printf("lpText: %s\n", lpText);
+    printf("lpCaption: %s\n", lpCaption);
+    printf("\n\n******************************************\n");
+    return OldMessageBoxA(NULL, "new MessageBoxA", "Hooked", MB_OK);
+}
+
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        DisableThreadLibraryCalls(hModule);
+        DetourTransactionBegin();
+        DetourUpdateThread(GetCurrentThread());
+        DetourAttach(&(PVOID&)OldMessageBoxA, NewMessageBoxA);
+        DetourTransactionCommit();
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        DetourTransactionBegin();
+        DetourUpdateThread(GetCurrentThread());
+        DetourDetach(&(PVOID&)OldMessageBoxA, NewMessageBoxA);
+        DetourTransactionCommit();
+        break;
+    }
+    return TRUE;
+}
+

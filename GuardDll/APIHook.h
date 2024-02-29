4c504a3db1d3db02ca0f20d4a3adb31118766113
martin <13389882+martin5206@user.noreply.gitee.com>
2024-02-29 03:30:58 +0000

    update GuardDll/APIHook.h.
    Signed-off-by: martin <13389882+martin5206@user.noreply.gitee.com>

--- GuardDll/APIHook.h
+++ GuardDll/APIHook.h
@@ -4,7 +4,146 @@
 #define _API_HOOK_H
 
 #include <Windows.h>
-
+//消息框MessageBox操作的hook定义
 extern int (WINAPI* OldMessageBoxA)(_In_opt_ HWND hWnd, _In_opt_ LPCSTR lpText, _In_opt_ LPCSTR lpCaption, _In_ UINT uType);
 
 extern "C" __declspec(dllexport) int WINAPI NewMessageBoxA(_In_opt_ HWND hWnd, _In_opt_ LPCSTR lpText, _In_opt_ LPCSTR lpCaption, _In_ UINT uType);
+
+extern int (WINAPI* OldMessageBoxW)(_In_opt_ HWND hWnd, _In_opt_ LPCSTR lpText, _In_opt_ LPCSTR lpCaption, _In_ UINT uType);
+
+extern "C" __declspec(dllexport) int WINAPI NewMessageBoxW(_In_opt_ HWND hwnd, _In_opt_ LPCSTR lpText, _In_opt_ LPCSTR lpCaption, _In_ UINT uType);
+
+//文件操作API的hook定义
+extern  HANDLE(WINAPI* OldCreateFile)(
+    LPCWSTR               lpFileName,
+    DWORD                 dwDesiredAccess,
+    DWORD                 dwShareMode,
+    LPSECURITY_ATTRIBUTES lpSecurityAttributes,
+    DWORD                 dwCreationDisposition,
+    DWORD                 dwFlagsAndAttributes,
+    HANDLE                hTemplateFile
+);
+
+extern "C" __declspec(dllexport)HANDLE WINAPI NewCreateFile(
+    LPCWSTR               lpFileName,
+    DWORD                 dwDesiredAccess,
+    DWORD                 dwShareMode,
+    LPSECURITY_ATTRIBUTES lpSecurityAttributes,
+    DWORD                 dwCreationDisposition,
+    DWORD                 dwFlagsAndAttributes,
+    HANDLE                hTemplateFile
+);
+
+extern BOOL(WINAPI* OldWriteFile)(
+    HANDLE       hFile,
+    LPCVOID      lpBuffer,
+    DWORD        nNumberOfBytesToWrite,
+    LPDWORD      lpNumberOfBytesWritten,
+    LPOVERLAPPED lpOverlapped
+);
+
+extern "C" __declspec(dllexport)BOOL WINAPI NewReadFile(
+    HANDLE       hFile,
+    LPVOID       lpBuffer,
+    DWORD        nNumberOfBytesToRead,
+    LPDWORD      lpNumberOfBytesRead,
+    LPOVERLAPPED lpOverlapped
+);
+
+//堆操作API的hook定义
+extern HANDLE(WINAPI* OldHeapCreate)(DWORD fIOoptions, SIZE_T dwInitialSize, SIZE_T dwMaximumSize);
+
+extern "C" __declspec(dllexport)HANDLE WINAPI NewHeapCreate(DWORD fIOoptions, SIZE_T dwInitialSize, SIZE_T dwMaximumSize);
+
+extern BOOL(WINAPI* OldHeapDestory)(HANDLE heap);
+
+extern "C" __declspec(dllexport) BOOL WINAPI NewHeapDestory(HANDLE hHeap);
+
+extern BOOL(WINAPI* OldHeapFree)(HANDLE hHeap, DWORD dwFlags, _Frees_ptr_opt_ LPVOID lpMem);
+
+extern "C" __declspec(dllexport) BOOL WINAPI NewHeapFree(HANDLE hHeap, DWORD dwFlags, _Frees_ptr_opt_ LPVOID lpMem);
+
+extern LPVOID(WINAPI* OldHeapAlloc)(HANDLE hHeap,DWORD  dwFlags,SIZE_T dwBytes);
+
+extern "C" __declspec(dllexport) LPVOID WINAPI NewHeapAlloc(HANDLE hHeap, DWORD  dwFlags, SIZE_T dwBytes);
+
+//注册表API的hook定义
+
+//创建或打开注册表键
+extern LSTATUS(WINAPI* OldRegCreateKeyEx)(
+    HKEY                        hKey,
+    LPCTSTR                     lpSubKey,
+    DWORD                       Reserved,
+    LPTSTR                      lpClass,
+    DWORD                       dwOptions,
+    REGSAM                      samDesired,
+    const LPSECURITY_ATTRIBUTES lpSecurityAttributes,
+    PHKEY                       phkResult,
+    LPDWORD                     lpdwDisposition
+);
+
+extern "C" __declspec(dllexport)LSTATUS WINAPI NewRegCreateKeyEx(
+    HKEY                        hKey,
+    LPCWSTR                     lpSubKey,
+    DWORD                       Reserved,
+    LPWSTR                      lpClass,
+    DWORD                       dwOptions,
+    REGSAM                      samDesired,
+    const LPSECURITY_ATTRIBUTES lpSecurityAttributes,
+    PHKEY                       phkResult,
+    LPDWORD                     lpdwDisposition
+);
+//用于在注册表中设置键值的函数
+extern LSTATUS(WINAPI* OldRegSetValueEx)(
+    HKEY       hKey,
+    LPCWSTR    lpValueName,
+    DWORD      Reserved,
+    DWORD      dwType,
+    const BYTE* lpData,
+    DWORD      cbData
+);
+
+extern "C" __declspec(dllexport)LSTATUS WINAPI NewRegSetValueEx(
+    HKEY       hKey,
+    LPCWSTR    lpValueName,
+    DWORD      Reserved,
+    DWORD      dwType,
+    const BYTE * lpData,
+    DWORD      cbData
+);
+//关闭一个打开的注册表键的
+extern LSTATUS(WINAPI* OldRegCloseKey)(HKEY hKey);
+
+extern "C" __declspec(dllexport)LSTATUS WINAPI NewRegCloseKey(HKEY hKey);
+//用于打开注册表中的一个子键
+extern LSTATUS(WINAPI* OldRegOpenKeyEx)(
+    HKEY    hKey,
+    LPCWSTR lpSubKey,
+    DWORD   ulOptions,
+    REGSAM  samDesired,
+    PHKEY   phkResult
+);
+
+extern "C" __declspec(dllexport)LSTATUS WINAPI NewRegOpenKeyEx(
+    HKEY    hKey,
+    LPCWSTR lpSubKey,
+    DWORD   ulOptions,
+    REGSAM  samDesired,
+    PHKEY   phkResult
+);
+//删除键值
+extern LSTATUS(WINAPI* OldRegDeleteValue)(
+    HKEY    hKey,
+    LPCWSTR lpValueName
+);
+
+extern "C" __declspec(dllexport)LSTATUS WINAPI NewRegDeleteValue(
+    HKEY    hKey,
+    LPCWSTR lpValueName
+);
+
+
+
+
+
+

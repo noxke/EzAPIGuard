63bbc3d7d79c7e92ee8af131b9ac37974c2d1d9e
xketx <zpengkee@gmail.com>
2024-03-02 16:17:30 +0800

    gui界面绘制完成

--- EzAPIGuardGUI/main.py
+++ EzAPIGuardGUI/main.py
@@ -1,3 +1,10 @@
-if __name__ == "__main__":
-    input()
-    print("run")
+# EzAPIGuard 界面main
+
+from Scripts.ui import MainWindow
+from PyQt6.QtWidgets import QApplication
+import sys
+
+app = QApplication(sys.argv)
+window = MainWindow.Ui_MainWindow()
+window.show()
+sys.exit(app.exec())
